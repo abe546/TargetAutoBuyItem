e9ff9ec19abb3356c60471ddfc78838c268dfd46
abe546 <abe546@live.com>
2020-12-19 19:14:47 -0500

    Increase wait to 1 minute

--- src/main/java/AmazonOptions.java
+++ src/main/java/AmazonOptions.java
@@ -13,11 +13,11 @@ public class AmazonOptions
 
         int waitCount =0;
 
-        while(waitCount < 60 && driver.findElements(By.id(AMAZON_BUY_NOW_BUTTON_ID)).isEmpty())
+        while(waitCount < 120 && driver.findElements(By.id(AMAZON_BUY_NOW_BUTTON_ID)).isEmpty())
         {
             Thread.sleep(500);
 
-            if(waitCount == 59)
+            if(waitCount == 119)
             {
                 System.out.println("Couldn't find buy now button, out of stock. Refreshing page to try again "+ Instant.now());
                 waitCount = 0;
