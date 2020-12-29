882b8776255c560d94e2d15ce2e2e6bd5c0c97c2
abe546 <abe546@live.com>
2020-12-29 10:49:30 -0500

    Add more logging. Also added error resolution routine for Target when trying to add item to cart.

--- src/main/java/TargetOptions.java
+++ src/main/java/TargetOptions.java
@@ -16,18 +16,29 @@ import static util.UtilityStrings.*;
 public class TargetOptions {
 
     private static Logger logger = Logger.getLogger(TargetOptions.class);
+    private String BASE_URL = null;
     private WebDriverWait wait;
+
+    public TargetOptions() {
+
+    }
+
+    public TargetOptions(String baseUrl) {
+        BASE_URL = baseUrl;
+    }
+
     public WebDriver pickUpFromStore(WebDriver driver) throws InterruptedException {
+
+        ifErrorExistsClickOkAndRefesh(driver);
+
         //Set max timeout to be 20 seconds
 
-        int waitCount =0;
+        int waitCount = 0;
 
         //We will loop indefinitely, forever, until the pick up item button is available
-        while (waitCount <20 && driver.findElements(By.cssSelector(TARGET_ORDER_PICKUP_CSS)).isEmpty())
-        {
-            if(waitCount == 19)
-            {
-                System.out.println("Couldn't find pick up button, out of stock. Refreshing page to try again "+ Instant.now());
+        while (waitCount < 20 && driver.findElements(By.cssSelector(TARGET_ORDER_PICKUP_CSS)).isEmpty()) {
+            if (waitCount == 19) {
+                System.out.println("Couldn't find pick up button, out of stock. Refreshing page to try again " + Instant.now());
                 waitCount = 0;
                 //refresh the page and try again
                 driver.navigate().refresh();
@@ -41,33 +52,40 @@ public class TargetOptions {
         WebElement pickUpButton = driver.findElement(By.cssSelector(TARGET_ORDER_PICKUP_CSS));
 
         pickUpButton.click();
+        logger.info("PICKING UP FROM STORE : "+Instant.now());
+        while(ifErrorExistsClickOkAndRefesh(driver))
+        {
+            pickUpButton.click();
+            logger.info("PICKING UP FROM STORE : "+Instant.now());
+            //Sleep 8 seconds
+            Thread.sleep(8000);
+        }
 
         return driver;
     }
 
     public WebDriver viewCartAndCheckOut(WebDriver driver) throws InterruptedException {
+
         wait = new WebDriverWait(driver, 10);
 
         WebElement viewCartCheckOut;
 
         //Try to get checkoutButton for 5 seconds
-        int waitCount =0;
-        while(driver.findElements(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS)).isEmpty() && waitCount < 20)
-        {
+        int waitCount = 0;
+        while (driver.findElements(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS)).isEmpty() && waitCount < 20) {
             logger.info("ATTEMPTING TO WAIT FOR CHECKOUT BUTTON");
             Thread.sleep(500);
             waitCount++;
         }
 
 
-        if(driver.findElements(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS)).isEmpty()) {
+        if (driver.findElements(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS)).isEmpty()) {
 
             //View cart not available, attempt to decline coverage AND THEN click checkout
 
             wait.until(presenceOfElementLocated(By.cssSelector(TARGET_DECLINE_COVERAGE)));
 
-            if(driver.findElements(By.cssSelector(TARGET_DECLINE_COVERAGE)).isEmpty())
-            {
+            if (driver.findElements(By.cssSelector(TARGET_DECLINE_COVERAGE)).isEmpty()) {
                 logger.error("VIEW CART NOT AVAILABLE AND DECLINE NOT PRESENT ERROR");
                 throw new IllegalArgumentException("NO VIEW CART, NO DECLINE BUTTON");
             }
@@ -75,13 +93,14 @@ public class TargetOptions {
             WebElement declineCoverage = driver.findElement(By.cssSelector(TARGET_DECLINE_COVERAGE));
 
             declineCoverage.click();
-
+            logger.info("DECLINING COVERAGE : "+Instant.now());
             wait.until(presenceOfElementLocated(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS)));
         }
 
         viewCartCheckOut = driver.findElement(By.cssSelector(TARGET_VIEW_CART_AND_CHECKOUT_CSS));
 
         viewCartCheckOut.click();
+        logger.info("VIEW CART AND CHECKOUT : "+Instant.now());
 
         return driver;
     }
@@ -96,8 +115,7 @@ public class TargetOptions {
                 .findElement(By.xpath(TARGET_QUANTITY_DROP_DOWN_XPATH))
                 .findElements(By.tagName(TARGET_QUANTITY_OPTION_TAG));
 
-        while (qtyDropDownInfo.isEmpty())
-        {
+        while (qtyDropDownInfo.isEmpty()) {
             //Wait five seconds, then refresh
             driver.navigate().refresh();
 
@@ -114,17 +132,17 @@ public class TargetOptions {
 
         WebElement qtyDropDown = driver.findElement(By.cssSelector(TARGET_QUANTITY_DROP_DOWN_CSS));
 
-        if(qtyDropDown == null)
-        {
+        if (qtyDropDown == null) {
             //issue
-            logger.error("NOT ABLE TO INTERACT WITH TARGET_VIEW_CART_AND_CHECKOUT_CSS - "+TARGET_QUANTITY_DROP_DOWN_CSS);
+            logger.error("NOT ABLE TO INTERACT WITH TARGET_VIEW_CART_AND_CHECKOUT_CSS - " + TARGET_QUANTITY_DROP_DOWN_CSS);
 
             //TODO add remediation step
         }
 
         qtyDropDown.click();
         //Click the last list entry
-        qtyDropDownInfo.get(qtyDropDownInfo.size()-1).click();
+        qtyDropDownInfo.get(qtyDropDownInfo.size() - 1).click();
+        logger.info("SELECTING QUANTITY : "+Instant.now());
 
         return driver;
     }
@@ -139,17 +157,18 @@ public class TargetOptions {
         Thread.sleep(500);
 
         checkOutButton.click();
+        logger.info("CLICKING CHECKOUT BUTTON : "+Instant.now());
 
-        int count =0;
+        int count = 0;
 
-        while (!driver.findElements(By.cssSelector(TARGET_CHECKOUT_CART_CSS)).isEmpty())
-        {
+        while (!driver.findElements(By.cssSelector(TARGET_CHECKOUT_CART_CSS)).isEmpty()) {
             checkOutButton.click();
+            logger.info("CLICKING CHECKOUT BUTTON : "+Instant.now());
             //Click every 250 milliseconds
             Thread.sleep(250);
             count++;
 
-            if(count == 120) //If we've been clicking checkout cart for 30 seconds refresh
+            if (count == 120) //If we've been clicking checkout cart for 30 seconds refresh
             {
                 driver.navigate().refresh();
                 count = 0;
@@ -172,8 +191,8 @@ public class TargetOptions {
 
         Thread.sleep(500);
 
-        for(char character : TARGET_MASTER_USERNAME.toCharArray()) {
-            userNameTextField.sendKeys(""+character);
+        for (char character : TARGET_MASTER_USERNAME.toCharArray()) {
+            userNameTextField.sendKeys("" + character);
             Thread.sleep(250);
         }
 
@@ -185,8 +204,8 @@ public class TargetOptions {
 
         Thread.sleep(500);
 
-        for(char character : TARGET_MASTER_PASSWORD.toCharArray()) {
-            passwordTextField.sendKeys(""+character);
+        for (char character : TARGET_MASTER_PASSWORD.toCharArray()) {
+            passwordTextField.sendKeys("" + character);
             Thread.sleep(250);
         }
 
@@ -208,14 +227,12 @@ public class TargetOptions {
 
     public WebDriver inputCreditCardCvv(WebDriver driver) throws InterruptedException {
 
-        int waitCount =0;
+        int waitCount = 0;
 
-        while(waitCount < 20 && driver.findElements(By.id(TARGET_CC_CVV_ID)).isEmpty())
-        {
+        while (waitCount < 10 && driver.findElements(By.id(TARGET_CC_CVV_ID)).isEmpty()) {
             Thread.sleep(500);
 
-            if(waitCount == 19)
-            {
+            if (waitCount == 9) {
                 //Its possibly that ccv is cached, since it did not pop up as an option
                 //So we will return and assume it is working, if not then Target may force
                 //textFields of a credit card to be populated. This must be done manually.
@@ -230,13 +247,18 @@ public class TargetOptions {
         Thread.sleep(500);
 
         ccCvv.click();
+        logger.info("CLICKING CVV BOX : "+Instant.now());
 
         Thread.sleep(500);
 
-        for(char character : TARGET_CC_CVV_SECRET.toCharArray()) {
-            ccCvv.sendKeys(""+character);
+        logger.info("CVV SENDING KEYS : "+Instant.now());
+
+        for (char character : TARGET_CC_CVV_SECRET.toCharArray()) {
+            ccCvv.sendKeys("" + character);
             Thread.sleep(250);
         }
+
+        logger.info("CVV SENT KEYS : "+Instant.now());
 
         Thread.sleep(500);
 
@@ -253,24 +275,49 @@ public class TargetOptions {
 
         Thread.sleep(1000);
 
+        logger.info("CLICKING PLACE ORDER : "+Instant.now());
         placeOrder.click();
-
-        int count =0;
-        while (!driver.findElements(By.xpath(TARGET_PLACE_ORDER_XPATH)).isEmpty())
-        {
+        logger.info("CLICKED PLACE ORDER : "+Instant.now());
+        Thread.sleep(5000);
+        int count = 0;
+        while (!driver.findElements(By.xpath(TARGET_PLACE_ORDER_XPATH)).isEmpty()) {
             //As long as place order button is visible, click it every 250 milliseconds
+            logger.info("CLICKING PLACE ORDER : "+Instant.now());
             placeOrder.click();
-            Thread.sleep(250);
+            logger.info("CLICKED PLACE ORDER : "+Instant.now());
+            Thread.sleep(500);
 
             //After 5 minutes of clicking, refresh the page
-            if(count == 1200)
-            {
-                count =0;
+            if (count == 600) {
+                count = 0;
                 driver.navigate().refresh();
             }
 
         }
 
         return driver;
+    }
+
+    /**
+     * If an error button exists click OK
+     *
+     * @param driver
+     * @return
+     */
+    public boolean ifErrorExistsClickOkAndRefesh(WebDriver driver) throws InterruptedException {
+        if (!driver.findElements(By.xpath(TARGET_CONTINUE_ERROR_BUTTON_XPATH)).isEmpty()) {
+            logger.info("ERROR ENCOUNTERED, REFRESHING");
+            WebElement errorButton = driver.findElement(By.xpath(TARGET_CONTINUE_ERROR_BUTTON_XPATH));
+
+            errorButton.click();
+
+            Thread.sleep(500);
+            logger.info("REFRESHING");
+            driver.navigate().refresh();
+
+            return true; //There was an error
+        }
+
+        return false; //No error
     }
 }
